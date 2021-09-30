9c267f9b3d600f7c7257ef7364d9f6f44f2ee4bf
KritantaDev <KritantaDevelopment@gmail.com>
2021-09-30 04:36:07 -0400

    Move kimg4 to its own package

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
 setup(name='ktool',
-      version='0.9.0',
+      version='0.9.1',
       description='Static mach-o/img4 analysis tool.',
       long_description='file: README.md',
       long_description_content_type='text/markdown',
       python_requires='>=3.10',
       author='kritanta',
       url='https://github.com/kritantadev/ktool',
-      install_requires=['pyaes'],
-      packages=['kimg4', 'kmacho', 'ktool'],
+      install_requires=['pyaes', 'kimg4'],
+      packages=['kmacho', 'ktool'],
       package_dir={
-            'kimg4': 'src/kimg4',
-            'kmacho':'src/kmacho',
+            'kmacho': 'src/kmacho',
             'ktool': 'src/ktool'
       },
       classifiers=[
