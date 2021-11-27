de9c443f8feb6a4e203b043812e924af9e12ffbe
KritantaDev <KritantaDevelopment@gmail.com>
2021-11-27 10:18:56 -0500

    Fix broken info command

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='k2l',
-      version='0.15.5',
+      version='0.15.6',
       description='Static mach-o/img4 analysis tool.',
       long_description=long_description,
       long_description_content_type='text/markdown',
