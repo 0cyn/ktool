8921d26e8f4101afa053fd150567e01fe7f6132d
KritantaDev <KritantaDevelopment@gmail.com>
2021-11-27 08:22:49 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='k2l',
-      version='0.15.3',
+      version='0.15.4',
       description='Static mach-o/img4 analysis tool.',
       long_description='file: README.md',
       long_description_content_type='text/markdown',
