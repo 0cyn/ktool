16fe8cd009576a82b3a15d54558ffa00ae6eac63
kat <katherine@vector35.com>
2022-05-14 17:37:52 -0400

    Update Module

--- src/ktool/__init__.py
+++ src/ktool/__init__.py
@@ -1,8 +1,11 @@
-from ktool.ktool import load_image, load_objc_metadata, generate_headers, generate_text_based_stub, load_macho_file, macho_verify, reload_image, macho_combine
+from ktool.ktool import load_image, load_objc_metadata, generate_headers, generate_text_based_stub, load_macho_file, \
+    macho_verify, reload_image, macho_combine
 
 from ktool.objc import ObjCImage
-from ktool.loader import MachOImageLoader, Image
-from ktool.macho import Slice, MachOFile, MachOFileType, Segment, Section
+from ktool.loader import MachOImageLoader
+from ktool.image import Image
+from ktool.macho import Slice, MachOFile, MachOFileType, Segment, Section, MachOImageHeader
+
 try:
     from ktool.headers import HeaderGenerator, Header
 except ModuleNotFoundError:
